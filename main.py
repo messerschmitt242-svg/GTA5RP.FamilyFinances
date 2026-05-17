51f441303803b60a5f5e515c37e66917a0629d05
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-17 22:41:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from core.bot import WayneBot
 from core.config import load_settings
 from core.database import Database
 
+import shutil
+
+shutil.copy("/data/family_finance.db", "family_finance.db")
 
 def main() -> None:
     settings = load_settings()
