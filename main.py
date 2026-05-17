f050a3dd6e4fd457e6bd88bf1c8d42aabfca87af
messerschmitt242-svg <messerschmitt242@gmail.com>
2026-05-17 22:54:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from core.bot import WayneBot
 from core.config import load_settings
 from core.database import Database
 
+import debug_volume
+
 def main() -> None:
     settings = load_settings()
     db = Database(settings.database_url)
